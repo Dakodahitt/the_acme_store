a7323051df9a9fa586e91e3b1dec738243008985
Dakoda hitt <dakoda@MacBook-Air.local>
2024-05-21 17:58:00 -0600

    app's get made

---
+++ index.js
@@ -0,0 +1,38 @@
+const {
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  fetchUsers,
+  fetchProducts,
+  createFavorite,
+  fetchFavorites,
+  destroyFavorite,
+} = require("./db");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/api/users", async (req, res, next) => {
+  try {
+    res.send(await fetchUsers());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/api/products", async (req, res, next) => {
+  try {
+    res.send(await fetchProducts());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/api/users/:id/favorites", async (req, res, next) => {
+  try {
+    res.send(await fetchFavorites(req.params.id));
+  } catch (ex) {
+    next(ex);
+  }
+});
