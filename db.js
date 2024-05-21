679d96f6133ec72729bd818201d27813bb17e0fa
Dakoda hitt <dakoda@MacBook-Air.local>
2024-05-21 17:57:18 -0600

    prostgres fix

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const pg = require("pg");
 const client = new pg.Client(
-  process.env.DATABASE_URL || "postgres://localhost/acme_talent_agency_db"
+  process.env.DATABASE_URL || "postgres://localhost/the_acme_store"
 );
 const uuid = require("uuid");
 const bcrypt = require("bcrypt");
@@ -27,4 +27,80 @@ const createTables = async () => {
     );
   `;
   await client.query(SQL);
+};
+const createUser = async ({ username, password }) => {
+  const SQL = `
+      INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *
+    `;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    username,
+    await bcrypt.hash(password, 5),
+  ]);
+  return response.rows[0];
+};
+
+const createProduct = async ({ name }) => {
+  const SQL = `
+      INSERT INTO products(id, name) VALUES ($1, $2) RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+
+const createFavorite = async ({ user_id, product_id }) => {
+  const SQL = `
+      INSERT INTO favorites(id, user_id, product_id) VALUES ($1, $2, $3) RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid.v4(), user_id, product_id]);
+  return response.rows[0];
+};
+
+const fetchUsers = async () => {
+  const SQL = `
+      SELECT id, username 
+      FROM users
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchProducts = async () => {
+  const SQL = `
+      SELECT *
+      FROM products
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchFavorites = async (user_id) => {
+  const SQL = `
+      SELECT *
+      FROM favorites
+      WHERE user_id = $1
+    `;
+  const response = await client.query(SQL, [user_id]);
+  return response.rows;
+};
+
+const destroyFavorite = async ({ user_id, id }) => {
+  const SQL = `
+      DELETE
+      FROM favorites
+      WHERE user_id = $1 AND id = $2
+    `;
+  await client.query(SQL, [user_id, id]);
+};
+
+module.exports = {
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  fetchUsers,
+  fetchProducts,
+  createFavorite,
+  fetchFavorites,
+  destroyFavorite,
 };
